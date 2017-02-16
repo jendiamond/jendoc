3ffd8b3f04ba61456323dad927f3f44e8d3da27c
jendiamond <thejendiamond@gmail.com>
2017-02-16 20:22:09 +0000

    Add Doc c model, controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,38 @@
+class DocsController < ApplicationController
+  def index
+  end
+
+  def new
+    @doc = Doc.new
+  end
+
+  def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_doc
+  end
+
+  def doc_params
+    params.require(:doc).permit(:title, :content)
+  end
+end
